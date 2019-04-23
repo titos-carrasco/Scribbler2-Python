49b2b95c9e8e531350f2c33e6db372200bb00426
titos-carrasco <titos.carrasco@gmail.com>
2019-04-22 21:28:25 -0400

    Fix install script

--- setup.py
+++ setup.py
@@ -20,13 +20,13 @@ if __name__ == "__main__":
     #here = path.abspath(path.dirname(__file__))
 
     setup(
-        name='S2-python',
-        version='3.2.4-pre',
+        name='S2-Python',
+        version='3.2.4',
         description='Library for controlling the Parallax Scribbler S2 robot using the Fluke2 card from BetterBots',
         #long_description=long_description,
         url='https://github.com/titos-carrasco/Scribbler2-Python',
         author='Roberto Carrasco',
-        author_email='titos.carrasco@gmal.com',
+        author_email='titos.carrasco@gmail.com',
         license='The MIT License (MIT)',
         classifiers=[
             'Development Status :: 5 - Production/STable',
@@ -41,5 +41,5 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3.6',
         ],
         keywords='robot education programming',
-        packages=['rcr','rcr.robots', 'rcr.robots.scribbler2', 'rcr.utils'],
+        packages=['rcr','rcr.robots', 'rcr.robots.scribbler2', 'rcr.robots.fluke2', 'rcr.robots.net2', 'rcr.utils'],
     )
